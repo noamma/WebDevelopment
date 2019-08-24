19fb14b2483e75776873ca4c50ec3b68a7a3ba70
Noam Marko <noamma@Noams-MacBook-Air.local>
2019-08-24 19:03:40 +0300

    fixing gitignore file for .DS_Store subfolders and butifing app.js

--- Udemy/Back-End-Development/Authentication/Secrets/app.js
+++ Udemy/Back-End-Development/Authentication/Secrets/app.js
@@ -27,76 +27,82 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.route("/").get((req, res)=>{res.render("home")});
+app.route("/").get((req, res) => {
+  res.render("home")
+});
 
-app.get("/register", (req, res)=>{res.render("register")});
+app.get("/register", (req, res) => {
+  res.render("register")
+});
 
-app.get("/secrets", (req, res)=>{
-  if(req.isAuthenticated()){
+app.get("/secrets", (req, res) => {
+  if (req.isAuthenticated()) {
     res.render("secrets");
-  } else{
+  } else {
     res.redirect("/login");
   }
 });
 
-app.post("/register",(req, res)=>{
+app.post("/register", (req, res) => {
   let username = req.body.username;
   let password = req.body.password;
-//  let password = md5(req.body.password);
-// bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
-//   if(err){
-//     console.log(err);
-//   }else{
-//     dbhelper.createNewUser(username, hash,(result)=>{
-//       if(result){
-//         console.log("Successfuly registerd new user");
-//         console.log("User Name: " + result.email);
-//         console.log("Password: *******");
-//         res.redirect("/secrets");
-//       }
-//     });
-//   }
-// });
-console.log("User name: " + username);
-console.log("Password: " + password.length);
-dbhelper.registerUser(username, password,(err, result)=>{
-  if(err){
-    console.log(err);
-    res.redirect("/register");
-}else{
-  passport.authenticate("local")(req, res, ()=>{
-    res.redirect("/secrets");
+  //  let password = md5(req.body.password);
+  // bcrypt.hash(req.body.password, saltRounds, (err, hash)=>{
+  //   if(err){
+  //     console.log(err);
+  //   }else{
+  //     dbhelper.createNewUser(username, hash,(result)=>{
+  //       if(result){
+  //         console.log("Successfuly registerd new user");
+  //         console.log("User Name: " + result.email);
+  //         console.log("Password: *******");
+  //         res.redirect("/secrets");
+  //       }
+  //     });
+  //   }
+  // });
+  console.log("User name: " + username);
+  console.log("Password: " + password.length);
+  dbhelper.registerUser(username, password, (err, result) => {
+    if (err) {
+      console.log(err);
+      res.redirect("/register");
+    } else {
+      passport.authenticate("local")(req, res, () => {
+        res.redirect("/secrets");
+      });
+    }
   });
-}
-});
 });
 
-app.get("/login", (req, res)=>{res.render("login")});
+app.get("/login", (req, res) => {
+  res.render("login")
+});
 
-app.post("/login", (req, res)=>{
+app.post("/login", (req, res) => {
   console.log("Please waite.. we are checking your credentials");
-  dbhelper.loginUser(req.body.username, req.body.password, req, res,(err, user)=>{
-    if(err){
+  dbhelper.loginUser(req.body.username, req.body.password, req, res, (err, user) => {
+    if (err) {
       console.log("login failed: " + err);
       res.redirect("/login");
-    }else{
+    } else {
       res.redirect("/secrets");
     }
   });
-  });
-  //const _password = md5(req.body.password);
-  // dbhelper.validateCredentials(_username, req.body.password, (result)=>{
-  //   if(result){
-  //     if (result === "reject"){
-  //       res.send("Login failed: bad Password");
-  //     }else{
-  //         res.render("secrets");
-  //     }
-  //   }else{
-  //     res.send("Login failed: No such User!");
-  //   }
-  // });
+});
+//const _password = md5(req.body.password);
+// dbhelper.validateCredentials(_username, req.body.password, (result)=>{
+//   if(result){
+//     if (result === "reject"){
+//       res.send("Login failed: bad Password");
+//     }else{
+//         res.render("secrets");
+//     }
+//   }else{
+//     res.send("Login failed: No such User!");
+//   }
+// });
 
-app.listen(3000, ()=>{
+app.listen(3000, () => {
   console.log("Server started at port 3000.");
 });
