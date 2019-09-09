16cc1d263600bb03ff67bc48bbd60fa99ff7a55f
Noam Marko <noamma@noams-air.home>
2019-09-09 23:11:32 +0300

    Adding QA page and adding missing packages for mongoose

--- Portal/app.js
+++ Portal/app.js
@@ -1,8 +1,11 @@
+
+
 const express = require('express');
 const ejs = require('ejs');
 const _ = require('lodash');
 const bodyParser = require('body-parser');
 const app = express();
+const dbhelper = require(__dirname + '/js/dbhelper');
 
 app.use(bodyParser.urlencoded({extended: true}), express.static('public'));
 
@@ -13,5 +16,7 @@ app.get('/',(req, res)=>{res.render('home', {title: 'Home'});});
 app.get('/hobbies',(req, res)=>{res.render('hobbies', {title: 'My hobbies'});});
 
 app.get('/about',(req, res)=>{res.render('about', {title: 'About Me'});});
+
+app.get('/qa',(req, res)=>{dbhelper.getQas((err, qaResult)=>{if (err){console.log(err);}else{console.log(qaResult);res.render('qa', {title: 'Q/A', qas: qaResult});}});});
 
 app.listen(3000, ()=>{console.log("server started at port: 3000");});
---
+++ Portal/js/dbhelper.js
@@ -0,0 +1,134 @@
+const mongoose = require("mongoose");
+//const encrypt = require("mongoose-encryption");
+//const bcrypt = require("bcrypt");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const dbcs = require(__dirname + "/dbcs.js");
+const connectionString = dbcs.getConnectionString();
+
+const _connect = ()=>{
+  //console.log(connectionstring);
+  mongoose.connect(connectionString,{
+    useNewUrlParser: true,
+    useFindAndModify: false});
+};
+
+// const _close = ()=>{
+//   mongoose.close();
+// }
+exports.createNewUser = (_email, _password, _callback)=>{
+  user = new User({
+    email: _email,
+    password: _password
+  });
+  _connect();
+  user.save((err, result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      //_close();
+      _callback(result);
+    }
+  });
+};
+
+exports.validateCredentials = (_username, _password, _callback)=>{
+  _connect();
+  User.findOne({email: _username}, (err, foundUser)=>{
+    if(err){
+      console.log(err);
+    }else if (foundUser){
+      console.log(foundUser.email);
+      bcrypt.compare(_password, foundUser.password, (err, result)=>{
+        if (result === true){
+          _callback(foundUser);
+        }else{
+          console.log("db message: login rejected due to password missmatch");
+          _callback("reject");
+        }
+      });
+    }else{
+      _callback(null);
+    }
+  });
+};
+
+exports.registerUser = (_username, _password, _callback)=>{
+  _connect();
+  User.register({username: _username}, _password, (err, user)=>{
+    if(err){
+      console.log("User.register produced an error: " + err);
+      _callback(err,null);
+    }else{
+      _callback(null,user);
+    }
+  });
+};
+
+exports.loginUser = (_username, _password, req, res,_callback)=>{
+  let user = new User({username: _username, password: _password});
+  _connect();
+  req.login(user, (err)=>{
+    if (err){
+      console.log("Req.login returned with an error: " + err);
+    }else{
+      passport.authenticate("local")(req, res, ()=>{
+          //console.log("authentication triggerd");
+          _callback(null, user);
+      });
+    }
+  });
+};
+
+exports.getQas = (_callback)=>
+{
+  _connect()
+  QA.find({},(err, foundQA)=>{
+  if (err){
+    _callback(err,null);
+  } else{
+    console.log("dbHelper result: " + foundQA);
+    _callback(null,foundQA);
+  }
+});};
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    //required: true},
+  },
+  password: {
+    type: String,
+    //required: true
+  }
+});
+
+const qaSchema = new mongoose.Schema({
+  q: String,
+  a: String
+});
+
+const QA = mongoose.model('QA', qaSchema);
+// , (err, user, info)=>{
+//   if(err){
+//     console.log(err);
+//     return err;
+//   }else{
+//     if(!user){
+//       console.log(info.message);
+//       return info.message;
+//     }
+//     console.log("authentication user: " + user);
+//     _callback(null, user);
+//   }
+// }
+
+//userSchema.plugin(encrypt, {secret: dbcs.getCryptoString(), encryptedFields: ['password']});
+userSchema.plugin(passportLocalMongoose);
+
+const User = new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
