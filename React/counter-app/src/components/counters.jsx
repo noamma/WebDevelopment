288aa4482d84cff94bdfca839538c6fd640850fd
Noam Marko <noamma@Noams-MacBook-Air.local>
2019-09-28 13:45:44 +0300

    move state and handle methudes to app js

--- React/counter-app/src/components/counters.jsx
+++ React/counter-app/src/components/counters.jsx
@@ -1,31 +1,20 @@
 //jshint esversion:6
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Counter from "./counter";
 
-class Counters extends Component{
-  state = {
-    counters: [
-      {id: 1, value:0},
-      {id: 2, value:4},
-      {id: 3, value:2},
-      {id: 4, value:5}
-    ]
-  };
-
-  handleDelete = counterId=>{
-    const counters = this.state.counters.filter(c=>c.id!==counterId);
-    this.setState({counters});
-  }
-  render(){
+class Counters extends Component {
+  render() {
+    const { onReset, counters, onDelete, onIncrement } = this.props;
     return (
       <React.Fragment>
-        {this.state.counters.map(counter=>(
+        <button onClick={onReset} className="btn btn-primary btn-sm m-2">Reset</button>
+        {counters.map(counter => (
           <Counter
-          key={counter.id}
-          onDelete = {this.handleDelete}
-          value={counter.value}
-          id={counter.id}
+            key={counter.id}
+            onDelete={onDelete}
+            onIncrement={onIncrement}
+            counter={counter}
           />
         ))}
       </React.Fragment>
